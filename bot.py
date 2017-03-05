285fed9c38976b23311fd9fbed2b14cb878c80f0
Dominic Jobin <dominic.jobin@gmail.com>
2017-03-05 01:15:18 -0500

    Try/except clause for -retry
    The bot won't crash if called with -retry when the botstate.pkl file
    doesnt exist

--- bot.py
+++ bot.py
@@ -296,8 +296,11 @@ if __name__ == "__main__":
         if argument == "-retry":
             stateFile = '%s/botstate.pkl' % (directory)
             
-            with open(stateFile, 'rb') as file:
-                bot = pickle.load(file)
-            
-            if bot.todoList != []:
-                bot.run()
+            try:
+                with open(stateFile, 'rb') as file:
+                    bot = pickle.load(file)
+                
+                if bot.todoList != []:
+                    bot.run()
+            except:
+                print("No need for bot to restart")
